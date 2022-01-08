ad1b4e3ff7954ce131c5c0eac45279866c9b5cb2
William isaque <williamisaque18@gmail.com>
2022-01-08 20:37:09 -0300

    metodo add

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,5 @@
 const app = require("../app");
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log(
-    ` database ${process.env.DB_DATA_BASE} `,
-    `user ${process.env.DB_USER}`,
-    `senha ${process.env.DB_PASS}`,
-    `hosts ${process.env.DB_HOST}`
-  );
-});
+app.listen(process.env.PORT || 3333, () =>
+  console.log("iniciado na porta 3333")
+);
